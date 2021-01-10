ec08be89c57be3940cbe082fcd444b7aadc85b13
William William <william.inxcorp@gmail.com>
2021-01-10 14:22:09 +0700

    change to production build

--- src/Asset/Constant/APIConstant.js
+++ src/Asset/Constant/APIConstant.js
@@ -1,12 +1,12 @@
-export const address = "https://dev-api.pikapp.id/";
+export const address = "https://api.pikapp.id/";
 
 var secretDev = "21f6fc80-cfdb-11ea-87d0-0242ac130003"
 var secretProd = "qaah4zq3cutmr36kqvq95qj5hax8f9ku25fv"
-export const secret = secretDev;
+export const secret = secretProd;
 
 var clientIdDev = "abf0e2a9-e9ee-440f-8563-94481c64b797"
 var clientIdProd = "d4aummjdkbzdsd46vygjvcy235ywmhkm"
-export const clientId = clientIdDev;
+export const clientId = clientIdProd;
 var googleKeyProd = "AIzaSyBMG_JpxmoPEBH6fpveaowU2vkWYrZEw1Y"
 export const googleKey = googleKeyProd;
 
