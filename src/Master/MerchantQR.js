97cadf2616ce34a32f5e3cc0dd72b7b597762a9a
Anggi Prastianto <anggiprastianto30@gmail.com>
2021-04-15 11:51:57 +0700

    refix QRcode route

--- src/Master/MerchantQR.js
+++ src/Master/MerchantQR.js
@@ -7,36 +7,14 @@ import Cookies from "js-cookie";
 import { useHistory } from "react-router-dom";
 
 const MerchantResto = (props) => {
-  // const [merchant, setmerchant] = useState([
-  //   {
-  //     address: "",
-  //     rating: "",
-  //     logo: "",
-  //     distance: "",
-  //     storeId: "",
-  //     storeName: "",
-  //     storeDesc: "",
-  //     storeImage: "",
-  //   },
-  // ]);
+  const [allMerchant, setallMerchant] = useState([])
+  const [merchant, setmerchant] = useState({});
   // const [longlatad, setlonglat] = useState("");
   const [longlat, setlonglat] = useState({});
   const [page, setpage] = useState(0);
   const [pageCond, setpageCond] = useState(false);
   let history = useHistory();
   const _isMounted = useRef(true)
-
-  // useEffect(() => {
-  //   if (props.coords) {
-  //     let latitude = props.coords.latitude;
-  //     let longitude = props.coords.longitude;
-  //     let longlat = { lat: latitude, lon: longitude };
-  //     console.log(latitude, longitude);
-  //     localStorage.setItem("longlat", JSON.stringify(longlat));
-  //   } else {
-  //     setlonglat("getloc succeed")
-  //   }
-  // }, []);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -54,31 +32,14 @@ const MerchantResto = (props) => {
     let longitude = position.coords.longitude
     let longlat = { lat: latitude, lon: longitude }
     console.log(latitude, longitude);
-    setlonglat({ lat: latitude, lon: longitude })
     localStorage.setItem("longlat", JSON.stringify(longlat))
+    reloadNextPageMerchant()
   }
 
-  // useEffect(() => {
-  //   if (props.coords) {
-  //     let latitude = props.coords.latitude;
-  //     let longitude = props.coords.longitude;
-  //     let longlat = { lat: latitude, lon: longitude };
-  //     console.log(latitude, longitude);
-  //     console.log(longlatad);
-  //     localStorage.setItem("longlat", JSON.stringify(longlat));
-  //     getMerchantData(latitude, longitude)
-  //   }
-  // });
-
-  useEffect(() => {
-    if (longlat.lat) {
-      getMerchantData(longlat.lat, longlat.lon)
-    }
-  });
-
-  const getMerchantData = (lat, lon) => {
+  const reloadNextPageMerchant = () => {
+    let longlatad = JSON.parse(localStorage.getItem("longlat"))
     let addressRoute =
-      address + "home/v2/merchant/" + lon + "/" + lat + "/ALL/";
+      address + "home/v2/merchant/" + longlatad.lon + "/" + longlatad.lat + "/ALL/";
     let uuid = uuidV4();
     uuid = uuid.replaceAll("-", "");
     const date = new Date().toISOString();
@@ -99,94 +60,111 @@ const MerchantResto = (props) => {
       }
     })
       .then((res) => {
-        let responseDatas = res.data.results;
-        Array.prototype.forEach.call(responseDatas, (dataMerch, indexMerch) => {
-          if (dataMerch.mid === props.match.params.mid) {
-            // condition where api response and params at url match
-            let stateData = []
-            stateData.push(dataMerch)
-            var currentMerchant = {
-              mid: "",
-              storeName: "",
-              storeDesc: "",
-              distance: "",
-              storeImage: "",
-              storeAdress: "",
-              storeRating: "",
-            };
-            currentMerchant.mid = dataMerch.mid;
-            currentMerchant.storeName = dataMerch.merchant_name;
-            currentMerchant.storeDesc = "Desc";
-            currentMerchant.distance = dataMerch.merchant_distance;
-            currentMerchant.storeImage = dataMerch.merchant_pict;
-            currentMerchant.storeAdress = dataMerch.merchant_address;
-            currentMerchant.storeRating = dataMerch.merchant_rating;
-            Cookies.set("currentMerchant", currentMerchant, { expires: 1 });
-            localStorage.setItem('selectedMerchant', JSON.stringify(stateData))
-            history.push(
-              "/store?mid=" +
-              dataMerch.mid +
-              "&table=" +
-              props.match.params.notab
-            );
-          } else {
-            if (indexMerch === responseDatas.length - 1) {
-              let nextPage = page
-              nextPage++
-              setpage(nextPage)
-            }
-          }
-        })
-
-        // responseDatas.forEach((dataMerch, indexMerch) => {
-        //   if (dataMerch.mid === props.match.params.mid) {
-        //     // condition where api response and params at url match
-        //     let stateData = []
-        //     stateData.push(dataMerch)
-        //     var currentMerchant = {
-        //       mid: "",
-        //       storeName: "",
-        //       storeDesc: "",
-        //       distance: "",
-        //       storeImage: "",
-        //       storeAdress: "",
-        //       storeRating: "",
-        //     };
-        //     currentMerchant.mid = dataMerch.mid;
-        //     currentMerchant.storeName = dataMerch.merchant_name;
-        //     currentMerchant.storeDesc = "Desc";
-        //     currentMerchant.distance = dataMerch.merchant_distance;
-        //     currentMerchant.storeImage = dataMerch.merchant_pict;
-        //     currentMerchant.storeAdress = dataMerch.merchant_address;
-        //     currentMerchant.storeRating = dataMerch.merchant_rating;
-        //     Cookies.set("currentMerchant", currentMerchant, { expires: 1 });
-        //     localStorage.setItem('selectedMerchant', JSON.stringify(stateData))
-        //     history.push(
-        //       "/store?mid=" +
-        //       dataMerch.mid +
-        //       "&table=" +
-        //       props.match.params.notab
-        //     );
-        //   } else {
-        //     if (indexMerch === responseDatas.length-1) {
-        //       let nextPage = page
-        //       nextPage++
-        //       setpage(nextPage)
-        //     }
-        //   }
-        // })
+        setallMerchant(res.data.results)
+        // setlonglat({ lat: latitude, lon: longitude })
       })
       .catch((err) => console.log(err));
   }
 
-  return <div></div>;
-};
+  useEffect(() => {
+    reloadNextPageMerchant()
+  }, [page]);
+  
+  const getMerchantLocalData = () => {
+    allMerchant.forEach((dataMerch, indexMerch) => {
+      if (!pageCond) {
+        if (dataMerch.mid === props.match.params.mid) {
+          // condition where api response and params at url match
+          setmerchant(dataMerch)
+          setpageCond(true)
+        } else {
+          if (indexMerch === allMerchant.length - 1) {
+            let nextPage = page
+            nextPage++
+            setpage(nextPage)
+          }
+        }
+      }
+    })
+  }
 
-// export default geolocated({
-//   positionOptions: {
-//     enableHighAccuracy: false,
-//   },
-//   userDecisionTimeout: 5000,
-// })(MerchantResto);
+  useEffect(() => {
+    getMerchantLocalData()
+  }, [allMerchant]);
+
+  // const getMerchantData = (lat, lon) => {
+  //   let addressRoute =
+  //     address + "home/v2/merchant/" + lon + "/" + lat + "/ALL/";
+  //   let uuid = uuidV4();
+  //   uuid = uuid.replaceAll("-", "");
+  //   const date = new Date().toISOString();
+
+  //   Axios(addressRoute, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "x-request-id": uuid,
+  //       "x-request-timestamp": date,
+  //       "x-client-id": clientId,
+  //       "token": "PUBLIC",
+  //       "category": "1",
+  //     },
+  //     method: "GET",
+  //     params: {
+  //       page: page,
+  //       size: 6
+  //     }
+  //   })
+  //     .then((res) => {
+  //       let responseDatas = res.data.results;
+  //       responseDatas.forEach((dataMerch, indexMerch) => {
+  //         if (!pageCond) {
+  //           if (dataMerch.mid === props.match.params.mid) {
+  //             // condition where api response and params at url match
+  //             setmerchant(dataMerch)
+  //             setpageCond(true)
+  //           } else {
+  //             if (indexMerch === responseDatas.length - 1) {
+  //               let nextPage = page
+  //               nextPage++
+  //               setpage(nextPage)
+  //             }
+  //           }
+  //         }
+  //       })
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
+
+  if (pageCond) {
+    let stateData = []
+    stateData.push(merchant)
+    var currentMerchant = {
+      mid: "",
+      storeName: "",
+      storeDesc: "",
+      distance: "",
+      storeImage: "",
+      storeAdress: "",
+      storeRating: "",
+    };
+    currentMerchant.mid = merchant.mid;
+    currentMerchant.storeName = merchant.merchant_name;
+    currentMerchant.storeDesc = "Desc";
+    currentMerchant.distance = merchant.merchant_distance;
+    currentMerchant.storeImage = merchant.merchant_pict;
+    currentMerchant.storeAdress = merchant.merchant_address;
+    currentMerchant.storeRating = merchant.merchant_rating;
+    Cookies.set("currentMerchant", currentMerchant, { expires: 1 });
+    localStorage.setItem('selectedMerchant', JSON.stringify(stateData))
+    history.push(
+      "/store?mid=" +
+      merchant.mid +
+      "&table=" +
+      props.match.params.notab
+    );
+  }
+
+  return <div>Halo</div>;
+};
 
 export default MerchantResto
