0aa5e473e0cff1fedca758389fbf1c46a3c840b1
Anggi Prastianto <anggiprastianto30@gmail.com>
2021-04-15 16:26:51 +0700

    testing aws image cors

--- src/Master/MerchantQR.js
+++ src/Master/MerchantQR.js
@@ -7,7 +7,6 @@ import Cookies from "js-cookie";
 import { useHistory } from "react-router-dom";
 
 const MerchantResto = (props) => {
-  const [allMerchant, setallMerchant] = useState([])
   const [merchant, setmerchant] = useState({});
   // const [longlatad, setlonglat] = useState("");
   const [longlat, setlonglat] = useState({});
@@ -32,14 +31,19 @@ const MerchantResto = (props) => {
     let longitude = position.coords.longitude
     let longlat = { lat: latitude, lon: longitude }
     console.log(latitude, longitude);
+    setlonglat({ lat: latitude, lon: longitude })
     localStorage.setItem("longlat", JSON.stringify(longlat))
-    reloadNextPageMerchant()
   }
 
-  const reloadNextPageMerchant = () => {
-    let longlatad = JSON.parse(localStorage.getItem("longlat"))
+  useEffect(() => {
+    if (longlat.lat) {
+      getMerchantData(longlat.lat, longlat.lon)
+    }
+  });
+
+  const getMerchantData = (lat, lon) => {
     let addressRoute =
-      address + "home/v2/merchant/" + longlatad.lon + "/" + longlatad.lat + "/ALL/";
+      address + "home/v2/merchant/" + lon + "/" + lat + "/ALL/";
     let uuid = uuidV4();
     uuid = uuid.replaceAll("-", "");
     const date = new Date().toISOString();
@@ -60,80 +64,63 @@ const MerchantResto = (props) => {
       }
     })
       .then((res) => {
-        setallMerchant(res.data.results)
-        // setlonglat({ lat: latitude, lon: longitude })
+        let responseDatas = res.data.results;
+        // Array.prototype.forEach.call(responseDatas, (dataMerch, indexMerch) => {
+        //   if (dataMerch.mid === props.match.params.mid) {
+        //     // condition where api response and params at url match
+        //     let stateData = []
+        //     stateData.push(dataMerch)
+        //     var currentMerchant = {
+        //       mid: "",
+        //       storeName: "",
+        //       storeDesc: "",
+        //       distance: "",
+        //       storeImage: "",
+        //       storeAdress: "",
+        //       storeRating: "",
+        //     };
+        //     currentMerchant.mid = dataMerch.mid;
+        //     currentMerchant.storeName = dataMerch.merchant_name;
+        //     currentMerchant.storeDesc = "Desc";
+        //     currentMerchant.distance = dataMerch.merchant_distance;
+        //     currentMerchant.storeImage = dataMerch.merchant_pict;
+        //     currentMerchant.storeAdress = dataMerch.merchant_address;
+        //     currentMerchant.storeRating = dataMerch.merchant_rating;
+        //     Cookies.set("currentMerchant", currentMerchant, { expires: 1 });
+        //     localStorage.setItem('selectedMerchant', JSON.stringify(stateData))
+        //     history.push(
+        //       "/store?mid=" +
+        //       dataMerch.mid +
+        //       "&table=" +
+        //       props.match.params.notab
+        //     );
+        //   } else {
+        //     if (indexMerch === responseDatas.length - 1) {
+        //       let nextPage = page
+        //       nextPage++
+        //       setpage(nextPage)
+        //     }
+        //   }
+        // })
+
+        responseDatas.forEach((dataMerch, indexMerch) => {
+          if (!pageCond) {
+            if (dataMerch.mid === props.match.params.mid) {
+              // condition where api response and params at url match
+              setmerchant(dataMerch)
+              setpageCond(true)
+            } else {
+              if (indexMerch === responseDatas.length - 1) {
+                let nextPage = page
+                nextPage++
+                setpage(nextPage)
+              }
+            }
+          }
+        })
       })
       .catch((err) => console.log(err));
   }
-
-  useEffect(() => {
-    reloadNextPageMerchant()
-  }, [page]);
-  
-  const getMerchantLocalData = () => {
-    allMerchant.forEach((dataMerch, indexMerch) => {
-      if (!pageCond) {
-        if (dataMerch.mid === props.match.params.mid) {
-          // condition where api response and params at url match
-          setmerchant(dataMerch)
-          setpageCond(true)
-        } else {
-          if (indexMerch === allMerchant.length - 1) {
-            let nextPage = page
-            nextPage++
-            setpage(nextPage)
-          }
-        }
-      }
-    })
-  }
-
-  useEffect(() => {
-    getMerchantLocalData()
-  }, [allMerchant]);
-
-  // const getMerchantData = (lat, lon) => {
-  //   let addressRoute =
-  //     address + "home/v2/merchant/" + lon + "/" + lat + "/ALL/";
-  //   let uuid = uuidV4();
-  //   uuid = uuid.replaceAll("-", "");
-  //   const date = new Date().toISOString();
-
-  //   Axios(addressRoute, {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "x-request-id": uuid,
-  //       "x-request-timestamp": date,
-  //       "x-client-id": clientId,
-  //       "token": "PUBLIC",
-  //       "category": "1",
-  //     },
-  //     method: "GET",
-  //     params: {
-  //       page: page,
-  //       size: 6
-  //     }
-  //   })
-  //     .then((res) => {
-  //       let responseDatas = res.data.results;
-  //       responseDatas.forEach((dataMerch, indexMerch) => {
-  //         if (!pageCond) {
-  //           if (dataMerch.mid === props.match.params.mid) {
-  //             // condition where api response and params at url match
-  //             setmerchant(dataMerch)
-  //             setpageCond(true)
-  //           } else {
-  //             if (indexMerch === responseDatas.length - 1) {
-  //               let nextPage = page
-  //               nextPage++
-  //               setpage(nextPage)
-  //             }
-  //           }
-  //         }
-  //       })
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
 
   if (pageCond) {
     let stateData = []
@@ -164,7 +151,7 @@ const MerchantResto = (props) => {
     );
   }
 
-  return <div>Halo</div>;
+  return <div></div>;
 };
 
 export default MerchantResto
--- src/View/Product/ProductView.js
+++ src/View/Product/ProductView.js
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 // import { address, clientId } from "../../Asset/Constant/APIConstant";
 // import { v4 as uuidV4 } from "uuid";
 // import sha256 from "crypto-js/hmac-sha256";
-// import Axios from "axios";
+import Axios from "axios";
 import Cookies from "js-cookie"
 import Storeimg from '../../Asset/Illustration/storeimg2.jpeg'
 import Storeimg2 from '../../Asset/Illustration/storeimg1.png'
@@ -198,11 +198,12 @@ class ProductView extends React.Component {
       firstShownProduct[indexcategProd].category_products = newFilter
     })
 
-    console.log('ahui');
-
     console.log(currentMerchant.storeImage);
+    let newImage = currentMerchant.storeImage
+    newImage = newImage.replace(/^https:\/\//i, 'http://')
+    console.log(newImage);
 
-    prominent(Storeimg, { amount: 3 }).then((color) => {
+    prominent(newImage, { amount: 3 }).then((color) => {
       // return RGB color for example [241, 221, 63]
       var merchantColor = rgbHex(color[0][0], color[0][1], color[0][2])
       var productColor = rgbHex(color[2][0], color[2][1], color[2][2])
