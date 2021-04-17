6c1da53e7351eb64b2d3e94dd449288cdc659c1f
Anastasya Putri <putri618@gmail.com>
2021-04-17 10:06:22 +0700

    add reset pin and change dependencies

--- src/View/ResetPin/ResetPinView.js
+++ src/View/ResetPin/ResetPinView.js
@@ -2,7 +2,6 @@ import React from "react";
 import {connect} from 'react-redux'
 import {LoadingButton, DoneLoad} from '../../Redux/Actions'
 import "../../Asset/scss/AuthenticationLayout.scss";
-import Paper from '@material-ui/core/Paper';
 import ResetPinDialog from '../../Component/Authentication/ResetPin/ResetPinDialog';
 
 class ResetPinView extends React.Component {
