1bba4f485224b51a464be3d9c9c53abf95e9f9dc
Anggi Prastianto <anggiprastianto30@gmail.com>
2021-04-17 12:40:27 +0700

    fix qrcode bug

--- src/Master/MerchantQR.js
+++ src/Master/MerchantQR.js
@@ -6,10 +6,8 @@ import Cookies from "js-cookie";
 import { useHistory } from "react-router-dom";
 
 const MerchantResto = (props) => {
-  const [merchant, setmerchant] = useState({});
+  const merchantID = props.match.params.mid
   const [longlat, setlonglat] = useState({});
-  const [page, setpage] = useState(0);
-  const [pageCond, setpageCond] = useState(false);
   let history = useHistory();
   const _isMounted = useRef(true)
 
@@ -40,60 +38,54 @@ const MerchantResto = (props) => {
   });
 
   const getMerchantData = (lat, lon) => {
-    let addressRoute = address + "home/v2/detail/merchant/" + lon + "/" + lat + "/"
+    let addressRoute = address + "/home/v2/detail/merchant/" + lon + "/" + lat + "/"
     let uuid = uuidV4();
     uuid = uuid.replaceAll("-", "");
     const date = new Date().toISOString();
 
-    console.log(addressRoute);
-    console.log(clientId);
-    console.log(uuid);
-    console.log(date);
-    console.log(props.match.params.mid);
+    Axios(addressRoute, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-request-id": uuid,
+        "x-request-timestamp": date,
+        "x-client-id": clientId,
+        "token": "PUBLIC",
+        "mid": merchantID,
+      },
+      method: "GET"
+    })
+      .then((res) => {
+        console.log(res.data.results);
+        let responseDatas = res.data.results;
 
-    // Axios(addressRoute, {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "x-request-id": uuid,
-    //     "x-request-timestamp": date,
-    //     "x-client-id": clientId,
-    //     "token": "PUBLIC",
-    //     "mid": props.match.params.mid,
-    //   },
-    //   method: "GET"
-    // })
-    //   .then((res) => {
-    //     console.log(res.data.results);
-    //     let responseDatas = res.data.results;
-
-    //     let stateData = []
-    //     stateData.push(responseDatas)
-    //     var currentMerchant = {
-    //       mid: "",
-    //       storeName: "",
-    //       storeDesc: "",
-    //       distance: "",
-    //       storeImage: "",
-    //       storeAdress: "",
-    //       storeRating: "",
-    //     };
-    //     currentMerchant.mid = responseDatas.mid;
-    //     currentMerchant.storeName = responseDatas.merchant_name;
-    //     currentMerchant.storeDesc = "Desc";
-    //     currentMerchant.distance = responseDatas.merchant_distance;
-    //     currentMerchant.storeImage = responseDatas.merchant_pict;
-    //     currentMerchant.storeAdress = responseDatas.merchant_address;
-    //     currentMerchant.storeRating = responseDatas.merchant_rating;
-    //     Cookies.set("currentMerchant", currentMerchant, { expires: 1 });
-    //     localStorage.setItem('selectedMerchant', JSON.stringify(stateData))
-    //     history.push(
-    //       "/store?mid=" +
-    //       responseDatas.mid +
-    //       "&table=" +
-    //       props.match.params.notab
-    //     );
-    //   })
-    //   .catch((err) => console.log(err));
+        // let stateData = []
+        // stateData.push(responseDatas)
+        // var currentMerchant = {
+        //   mid: "",
+        //   storeName: "",
+        //   storeDesc: "",
+        //   distance: "",
+        //   storeImage: "",
+        //   storeAdress: "",
+        //   storeRating: "",
+        // };
+        // currentMerchant.mid = responseDatas.mid;
+        // currentMerchant.storeName = responseDatas.merchant_name;
+        // currentMerchant.storeDesc = "Desc";
+        // currentMerchant.distance = responseDatas.merchant_distance;
+        // currentMerchant.storeImage = responseDatas.merchant_pict;
+        // currentMerchant.storeAdress = responseDatas.merchant_address;
+        // currentMerchant.storeRating = responseDatas.merchant_rating;
+        // Cookies.set("currentMerchant", currentMerchant, { expires: 1 });
+        // localStorage.setItem('selectedMerchant', JSON.stringify(stateData))
+        // history.push(
+        //   "/store?mid=" +
+        //   responseDatas.mid +
+        //   "&table=" +
+        //   props.match.params.notab
+        // );
+      })
+      .catch((err) => console.log(err));
   }
 
   return <div></div>;
