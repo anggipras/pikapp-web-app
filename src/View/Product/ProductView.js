894dd0a9d222baee4918c035def230d4013c7bb2
Anggi Prastianto <anggiprastianto30@gmail.com>
2021-04-22 16:46:06 +0700

    change dummy image to image response

--- src/View/Product/ProductView.js
+++ src/View/Product/ProductView.js
@@ -728,7 +728,7 @@ class ProductView extends React.Component {
                 return (
                   <div key={indprod} className='product-merchant' onClick={() => this.handleDetail(product)}>
                     <div className='product-img'>
-                      <img src={Productimage} className='product-imgContent' alt='' />
+                      <img src={product.foodImage} className='product-imgContent' alt='' />
                     </div>
 
                     <div className='product-detail-mob'>
