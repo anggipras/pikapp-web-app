942196684b0cd1b85dad7f36306a26c51a485410
Anggi Prastianto <anggiprastianto30@gmail.com>
2021-05-18 19:06:43 +0700

    mocking async fetch merchant list page

---
+++ src/_test_/Store/StoreTest.js
@@ -0,0 +1,44 @@
+import Axios from "axios";
+import { address, clientId } from "../../Asset/Constant/APIConstant";
+import { v4 as uuidV4 } from "uuid";
+
+export const fetchData = async () => {
+    let latitude = -6.28862
+    let longitude = 106.71789
+    let addressRoute = address + "home/v2/merchant/" + longitude + "/" + latitude + "/ALL/"
+
+    let uuid = uuidV4();
+    uuid = uuid.replace(/-/g, "");
+    const date = new Date().toISOString();
+
+    const allMerchantAPI = await Axios(addressRoute, {
+        headers: {
+            "Content-Type": "application/json",
+            "x-request-id": uuid,
+            "x-request-timestamp": date,
+            "x-client-id": clientId,
+            "token": "PUBLIC",
+            "category": "1",
+        },
+        method: "GET",
+        params: {
+            page: 0,
+            size: 6
+        }
+    })
+
+    let allData = allMerchantAPI.data.results
+    let realData = []
+    realData.push({
+        address: allData.merchant_address,
+        rating: allData.merchant_rating,
+        logo: allData.merchant_logo,
+        distance: allData.merchant_distance,
+        storeId: allData.mid,
+        storeName: allData.merchant_name,
+        storeDesc: "",
+        storeImage: allData.merchant_pict,
+    })
+
+    return realData
+}
