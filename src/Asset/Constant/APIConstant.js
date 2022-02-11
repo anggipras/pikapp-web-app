e053921ae6a39484e4fb3f09a597bb0f909aa42d
Anastasya Putri <putri618@gmail.com>
2022-02-11 04:48:07 +0000

    Update APIConstant.js

--- src/Asset/Constant/APIConstant.js
+++ src/Asset/Constant/APIConstant.js
@@ -1,19 +1,19 @@
-// export const address = "https://dev-api.pikapp.id/";
-export const address = "https://api.pikapp.id/";
+export const address = "https://dev-api.pikapp.id/";
+// export const address = "https://api.pikapp.id/";
 
 export const addressShipping = "https://dev-courier-api.pikapp.id";
 
 var secretDev = "21f6fc80-cfdb-11ea-87d0-0242ac130003"
 var secretProd = "qaah4zq3cutmr36kqvq95qj5hax8f9ku25fv"
-export const secret = secretProd;
+export const secret = secretDev;
 
 var clientIdDev = "abf0e2a9-e9ee-440f-8563-94481c64b797"
 var clientIdProd = "d4aummjdkbzdsd46vygjvcy235ywmhkm"
-export const clientId = clientIdProd;
+export const clientId = clientIdDev;
 // googleKeyDev need to activate billing which later should pay after expired
 var googleKeyProd = "AIzaSyBMG_JpxmoPEBH6fpveaowU2vkWYrZEw1Y"
 var googleKeyDev = "AIzaSyAikqroR5x4BrRv5Yj-tWgUcqvjEKUysh4"
-export const googleKey = googleKeyProd;
+export const googleKey = googleKeyDev;
 
 export const mapsApiKey = "AIzaSyAhpg4G_EANh5cM9sHUY04XZaTAJVrd0Us";
 
