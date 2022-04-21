dc6d8570fd6f40071bb7f75ef9557123685614fe
Anastasya Putri <putri618@gmail.com>
2022-04-22 04:30:26 +0700

    fixing condition status in button lacak and beri penilaian

--- src/View/OrderReceipt/OrderReceiptView.js
+++ src/View/OrderReceipt/OrderReceiptView.js
@@ -541,24 +541,21 @@ export class OrderReceiptView extends React.Component {
             {this.contentMainView()}
         </div>
         {
-          this.state.data[0].order_type === "DELIVERY" ?
-            this.state.data[0].order_status === "DELIVER" ?
-            <Link to="/tracking" style={{ textDecoration: "none" }}>
-              <div className="order-receipt-footer-sec">
-                <div className="order-receipt-buttonfooter" style={{backgroundColor: '#4bb7ac'}}>Lacak</div>
-              </div>
-            </Link>
-            :
-            this.state.data[0].order_status === "CLOSE" ?
+          this.state.data[0].order_status === "DELIVER" && this.state.data[0].order_type === "DELIVERY" ?
+          <Link to="/tracking" style={{ textDecoration: "none" }}>
             <div className="order-receipt-footer-sec">
-              <Link to={{ pathname: "/merchant/rating", state: { txnId : this.props.match.params.txnid }}} style={{ textDecoration: "none" }}>
-                <div className="order-receipt-buttonfooter" style={{backgroundColor: '#4bb7ac'}}>Beri Penilaian</div>
-              </Link>
+              <div className="order-receipt-buttonfooter" style={{backgroundColor: '#4bb7ac'}}>Lacak</div>
             </div>
-            :
-            <></>
-            :
-            <></>
+          </Link>
+          :
+          this.state.data[0].order_status === "CLOSE" ?
+          <div className="order-receipt-footer-sec">
+            <Link to={{ pathname: "/merchant/rating", state: { txnId : this.props.match.params.txnid }}} style={{ textDecoration: "none" }}>
+              <div className="order-receipt-buttonfooter" style={{backgroundColor: '#4bb7ac'}}>Beri Penilaian</div>
+            </Link>
+          </div>
+          :
+          <></>
         }
         
       </>
